21d4cd20f02e32bb362ac97294eef7bc6f06ee5f
geekthread <geekthread@users.noreply.github.com>
2026-05-04 20:05:05 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type=10
+float_type=10.1
+bool_type=False
+string_type="Ankit"
+list_type=["0","1"]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
